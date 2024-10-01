0b0dbddfca4685c3c38aa180d91a5073dceef00c
Oleksii Nazarenko <alexeybread81@gmail.com>
2024-10-01 17:31:12 +0100

    fix timings

--- lib/getCurrentMenuWeek.ts
+++ lib/getCurrentMenuWeek.ts
@@ -1,16 +1,16 @@
-import getCurrentWeekInYear from './getCurrentWeekInYear'
+import getCurrentWeekInYear from "./getCurrentWeekInYear";
 
 export default function getCurrentMenuWeek() {
-	const OFFSET = 0
-	const NUMBER_OF_WEEKS = 4
+  const OFFSET = 1;
+  const NUMBER_OF_WEEKS = 4;
 
-	const weekNumber = getCurrentWeekInYear()
+  const weekNumber = getCurrentWeekInYear();
 
-	let menuWeekNumber = (parseInt(weekNumber) % NUMBER_OF_WEEKS) + OFFSET
+  let menuWeekNumber = (parseInt(weekNumber + 3) % NUMBER_OF_WEEKS) + OFFSET;
 
-	if (menuWeekNumber > 4) {
-		menuWeekNumber -= 4
-	}
+  if (menuWeekNumber > 4) {
+    menuWeekNumber -= 4;
+  }
 
-	return menuWeekNumber
+  return menuWeekNumber;
 }
